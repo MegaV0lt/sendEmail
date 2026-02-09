9c7df7abe5d1654fed32f2dbe6268b4e6040d236
MegaV0lt <M_V0lt@mail.ru>
2026-02-09 08:22:33 +0100

    Add topical help system with color support to sendEmail.py
    - Implement --help and -h for main help display
    - Add --help <topic> for detailed help on: addressing, message, networking, output, misc
    - Integrate color codes (Colors class) for better readability
    - Topics include: addressing, message, networking, output, misc
    - Invalid topics show error message with valid options
    - Matches Perl version's help system functionality
    - Auto-show help when no arguments provided

--- sendEmail.py
+++ sendEmail.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 
-##############################################################################
+##############################################################################################
 ## sendEmail - Python3-Implementation
 ## Originally Written by: Brandon Zehm <caspian@dotconf.net>
 ## Modifications by: MegaV0lt @ github.com
-##   Convert original sendEmail.pl to Python 3
-##   Use Python's built-in email and mimetypes modules for message construction 
-##     and MIME type detection 
-## License: GNU General Public License v2 or later
-##############################################################################
+##  - Convert original sendEmail.pl to Python 3
+##  - Use Python's built-in email and mimetypes modules for message construction 
+##      and MIME type detection 
+##############################################################################################
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+## You should have received a copy of the GNU General Public License
+## along with this program. If not, see <http://www.gnu.org/licenses/>.
+##############################################################################################
 
 import sys
 import os
@@ -539,10 +549,314 @@ class EmailSender:
         except Exception as e:
             self.printmsg(f"ERROR => Failed to attach file {filepath}: {e}", 0)
 
+def show_help():
+    """Display main help message with colors"""
+    print(f"\n{Colors.BOLD}{PROGRAM_NAME}-{VERSION} by {AUTHOR_NAME} <{AUTHOR_EMAIL}>{Colors.NO_BOLD}\n")
+    print(f"Synopsis:  {PROGRAM_NAME} -f ADDRESS [options]\n")
+    
+    print(f"  {Colors.RED}Required:{Colors.NORMAL}")
+    print(f"    -f ADDRESS                from (sender) email address")
+    print(f"    * At least one recipient required via -t, -cc, or -bcc")
+    print(f"    * Message body required via -m, STDIN, or -o message-file=FILE\n")
+    
+    print(f"  {Colors.GREEN}Common:{Colors.NORMAL}")
+    print(f"    -t ADDRESS [ADDR ...]     to email address(es)")
+    print(f"    -u SUBJECT                message subject")
+    print(f"    -m MESSAGE                message body")
+    print(f"    -s SERVER[:PORT]          smtp mail relay, default is localhost:25\n")
+    
+    print(f"  {Colors.GREEN}Optional:{Colors.NORMAL}")
+    print(f"    -a   FILE [FILE ...]      file attachment(s)")
+    print(f"    -cc  ADDRESS [ADDR ...]   cc  email address(es)")
+    print(f"    -bcc ADDRESS [ADDR ...]   bcc email address(es)")
+    print(f"    -xu  USERNAME             username for SMTP authentication")
+    print(f"    -xp  PASSWORD             password for SMTP authentication\n")
+    
+    print(f"  {Colors.GREEN}Paranormal:{Colors.NORMAL}")
+    print(f"    -b BINDADDR[:PORT]        local host bind address")
+    print(f"    -l LOGFILE                log to the specified file")
+    print(f"    -v                        verbosity, use multiple times for greater effect")
+    print(f"    -q                        be quiet (i.e. no STDOUT output)")
+    print(f"    -o NAME=VALUE             advanced options, for details try: --help misc")
+    print(f"        -o message-content-type=<auto|text|html>")
+    print(f"        -o message-file=FILE         -o message-format=raw")
+    print(f"        -o message-header=HEADER     -o message-charset=CHARSET")
+    print(f"        -o reply-to=ADDRESS          -o timeout=SECONDS")
+    print(f"        -o username=USERNAME         -o password=PASSWORD")
+    print(f"        -o tls=<auto|yes|no>         -o fqdn=FQDN\n")
+    
+    print(f"  {Colors.GREEN}Extra:{Colors.NORMAL}")
+    print(f"    -dsn [OPTIONS]            enable Delivery Status Notification (DSN)")
+    print(f"                              DSN options: (S)uccess, (F)ailure, (D)elay")
+    print(f"                              default value for OPTIONS is 'sfd'")
+    print(f"    -dsnid [ID]               envelope ID for DSN requests, default is random\n")
+    
+    print(f"  {Colors.GREEN}Help:{Colors.NORMAL}")
+    print(f"    --help                    the helpful overview you're reading now")
+    print(f"    --help addressing         explain addressing and related options")
+    print(f"    --help message            explain message body input and related options")
+    print(f"    --help networking         explain -s, -b, etc")
+    print(f"    --help output             explain logging and other output options")
+    print(f"    --help misc               explain -o options, TLS, SMTP auth, and more\n")
+    sys.exit(0)
+
+def show_help_topic(topic):
+    """Display help for a specific topic with colors"""
+    if topic == 'addressing':
+        print(f"\n{Colors.BOLD}ADDRESSING DOCUMENTATION{Colors.NORMAL}\n")
+        
+        print(f"{Colors.GREEN}Addressing Options{Colors.NORMAL}")
+        print(f"Options related to addressing:")
+        print(f"    -f   ADDRESS")
+        print(f"    -t   ADDRESS [ADDRESS ...]")
+        print(f"    -cc  ADDRESS [ADDRESS ...]")
+        print(f"    -bcc ADDRESS [ADDRESS ...]")
+        print(f"    -o   reply-to=ADDRESS\n")
+        
+        print(f"-f ADDRESS")
+        print(f"    This required option specifies who the email is from, I.E. the sender's")
+        print(f"    email address.\n")
+        
+        print(f"-t ADDRESS [ADDRESS ...]")
+        print(f"    This option specifies the primary recipient(s).  At least one recipient")
+        print(f"    address must be specified via the -t, -cc. or -bcc options.\n")
+        
+        print(f"-cc ADDRESS [ADDRESS ...]")
+        print(f"    This option specifies the \"carbon copy\" recipient(s).  At least one")
+        print(f"    recipient address must be specified via the -t, -cc. or -bcc options.\n")
+        
+        print(f"-bcc ADDRESS [ADDRESS ...]")
+        print(f"    This option specifies the \"blind carbon copy\" recipient(s).  At least")
+        print(f"    one recipient address must be specified via the -t, -cc. or -bcc options.\n")
+        
+        print(f"-o reply-to=ADDRESS")
+        print(f"    This option specifies that an optional \"Reply-To\" address should be")
+        print(f"    written in the email's headers.\n")
+        
+        print(f"{Colors.GREEN}Email Address Syntax{Colors.NORMAL}")
+        print(f"Email addresses may be specified in one of two ways:")
+        print(f"    Full Name:     \"John Doe <john.doe@gmail.com>\"")
+        print(f"    Just Address:  \"john.doe@gmail.com\"\n")
+        
+        print(f"The \"Full Name\" method is useful if you want a name, rather than a plain")
+        print(f"email address, to be displayed in the recipient's From, To, or Cc fields")
+        print(f"when they view the message.\n")
+        
+        print(f"{Colors.GREEN}Multiple Recipients{Colors.NORMAL}")
+        print(f"The -t, -cc, and -bcc options each accept multiple addresses.  They may be")
+        print(f"specified by separating them by either a white space, comma, or semi-colon")
+        print(f"separated list.  You may also specify the -t, -cc, and -bcc options multiple")
+        print(f"times, each occurance will append the new recipients to the respective list.\n")
+        
+        print(f"Examples:")
+        print(f"  * Space separated list:")
+        print(f"    -t jane.doe@yahoo.com \"John Doe <john.doe@gmail.com>\"\n")
+        print(f"  * Semi-colon separated list:")
+        print(f"    -t \"jane.doe@yahoo.com; John Doe <john.doe@gmail.com>\"\n")
+        print(f"  * Comma separated list:")
+        print(f"    -t \"jane.doe@yahoo.com, John Doe <john.doe@gmail.com>\"\n")
+        print(f"  * Multiple -t, -cc, or -bcc options:")
+        print(f"    -t \"jane.doe@yahoo.com\" -t \"John Doe <john.doe@gmail.com>\"\n")
+    
+    elif topic == 'message':
+        print(f"\n{Colors.BOLD}MESSAGE DOCUMENTATION{Colors.NORMAL}\n")
+        
+        print(f"{Colors.GREEN}Message Options{Colors.NORMAL}")
+        print(f"Options related to the email message body:")
+        print(f"    -u  SUBJECT")
+        print(f"    -m  MESSAGE")
+        print(f"    -o  message-file=FILE")
+        print(f"    -o  message-content-type=<auto|text|html>")
+        print(f"    -o  message-header=EMAIL HEADER")
+        print(f"    -o  message-charset=CHARSET")
+        print(f"    -o  message-format=raw\n")
+        
+        print(f"-u SUBJECT")
+        print(f"    This option allows you to specify the subject for your email message.")
+        print(f"    It is not required that the subject be quoted, although it is recommended.")
+        print(f"    The subject will be read until an argument starting with a hyphen (-) is found.\n")
+        
+        print(f"-m MESSAGE")
+        print(f"    This option is one of three methods that allow you to specify the message")
+        print(f"    body for your email.  The message may be specified on the command line")
+        print(f"    with this -m option, read from a file with the -o message-file=FILE")
+        print(f"    option, or read from STDIN if neither of these options are present.\n")
+        
+        print(f"-o message-file=FILE")
+        print(f"    This option is one of three methods that allow you to specify the message")
+        print(f"    body for your email.  To use this option simply specify a text file")
+        print(f"    containing the body of your email message.\n")
+        
+        print(f"-o message-content-type=<auto|text|html>")
+        print(f"    This option allows you to specify the content-type of the email. If your")
+        print(f"    email message is an html message but is being displayed as a text message")
+        print(f"    just add \"-o message-content-type=html\" to the command line to force it")
+        print(f"    to display as an html message.\n")
+        
+        print(f"-o message-header=EMAIL HEADER")
+        print(f"    This option allows you to specify additional email headers to be included.")
+        print(f"    To add more than one message header simply use this option on the command")
+        print(f"    line more than once.  If you specify a message header that sendEmail would")
+        print(f"    normally generate the one you specified will be used in its place.\n")
+        
+        print(f"-o message-charset=CHARSET")
+        print(f"    This option allows you to specify the character-set for the message body.")
+        print(f"    The default is iso-8859-1.\n")
+        
+        print(f"-o message-format=raw")
+        print(f"    This option instructs sendEmail to assume the message (specified with -m,")
+        print(f"    read from STDIN, or read from the file specified in -o message-file=FILE)")
+        print(f"    is already a *complete* email message.  SendEmail will not generate any")
+        print(f"    headers and will transmit the message as-is to the remote SMTP server.\n")
+    
+    elif topic == 'networking':
+        print(f"\n{Colors.BOLD}NETWORKING DOCUMENTATION{Colors.NORMAL}\n")
+        
+        print(f"{Colors.GREEN}Networking Options{Colors.NORMAL}")
+        print(f"Options related to networking:")
+        print(f"    -s   SERVER[:PORT]")
+        print(f"    -b   BINDADDR[:PORT]")
+        print(f"    -o   tls=<auto|yes|no>")
+        print(f"    -o   timeout=SECONDS\n")
+        
+        print(f"-s SERVER[:PORT]")
+        print(f"    This option allows you to specify the SMTP server sendEmail should")
+        print(f"    connect to to deliver your email message to.  If this option is not")
+        print(f"    specified sendEmail will try to connect to localhost:25 to deliver")
+        print(f"    the message.  THIS IS MOST LIKELY NOT WHAT YOU WANT, AND WILL LIKELY")
+        print(f"    FAIL unless you have a email server running on your computer!\n")
+        
+        print(f"    Typically you will need to specify your company or ISP's email server.")
+        print(f"    For example, if you use CableOne you will need to specify:")
+        print(f"       -s mail.cableone.net\n")
+        
+        print(f"    If you have your own email server running on port 300 you would")
+        print(f"    probably use an option like this:")
+        print(f"       -s myserver.mydomain.com:300\n")
+        
+        print(f"-b BINDADDR[:PORT]")
+        print(f"    This option allows you to specify the local IP address (and optional")
+        print(f"    tcp port number) for sendEmail to bind to when connecting to the remote")
+        print(f"    SMTP server.  This useful for people who need to send an email from a")
+        print(f"    specific network interface or source address.\n")
+        
+        print(f"-o tls=<auto|yes|no>")
+        print(f"    This option allows you to specify if TLS should be enabled or disabled.")
+        print(f"    The default, auto, will use TLS automatically if your Python installation")
+        print(f"    has SSL support and the remote SMTP server supports TLS.\n")
+        
+        print(f"-o timeout=SECONDS")
+        print(f"    This option sets the timeout value in seconds used for all network reads,")
+        print(f"    writes, and a few other things.\n")
+    
+    elif topic == 'output':
+        print(f"\n{Colors.BOLD}OUTPUT DOCUMENTATION{Colors.NORMAL}\n")
+        
+        print(f"{Colors.GREEN}Output Options{Colors.NORMAL}")
+        print(f"Options related to output:")
+        print(f"    -l LOGFILE")
+        print(f"    -v")
+        print(f"    -q\n")
+        
+        print(f"-l LOGFILE")
+        print(f"    This option allows you to specify a log file to append to.  Every message")
+        print(f"    that is displayed to STDOUT is also written to the log file.  This may be")
+        print(f"    used in conjunction with -q and -v.\n")
+        
+        print(f"-q")
+        print(f"    This option tells sendEmail to disable printing to STDOUT.  In other")
+        print(f"    words nothing will be printed to the console.  This does not affect the")
+        print(f"    behavior of the -l or -v options.\n")
+        
+        print(f"-v")
+        print(f"    This option allows you to increase the debug level of sendEmail.  You may")
+        print(f"    either use this option more than once, or specify more than one v at a")
+        print(f"    time to obtain a debug level higher than one.\n")
+        
+        print(f"    Examples:")
+        print(f"        Specifies a debug level of 1:  -v")
+        print(f"        Specifies a debug level of 2:  -vv")
+        print(f"        Specifies a debug level of 2:  -v -v\n")
+        
+        print(f"    A debug level of one is recommended when doing any sort of debugging.")
+        print(f"    At that level you will see the entire SMTP transaction (except the body")
+        print(f"    of the email message), and hints will be displayed for most warnings and")
+        print(f"    errors.  The highest debug level is three.\n")
+    
+    elif topic == 'misc':
+        print(f"\n{Colors.BOLD}MISC DOCUMENTATION{Colors.NORMAL}\n")
+        
+        print(f"{Colors.GREEN}Misc Options{Colors.NORMAL}")
+        print(f"Options that don't fit anywhere else:")
+        print(f"    -a   ATTACHMENT [ATTACHMENT ...]")
+        print(f"    -xu  USERNAME")
+        print(f"    -xp  PASSWORD")
+        print(f"    -o   username=USERNAME")
+        print(f"    -o   password=PASSWORD")
+        print(f"    -o   tls=<auto|yes|no>")
+        print(f"    -o   timeout=SECONDS")
+        print(f"    -o   fqdn=FQDN\n")
+        
+        print(f"-a   ATTACHMENT [ATTACHMENT ...]")
+        print(f"    This option allows you to attach any number of files to your email message.")
+        print(f"    To specify more than one attachment, simply separate each filename with a")
+        print(f"    space.\n")
+        
+        print(f"-xu  USERNAME")
+        print(f"    Alias for -o username=USERNAME\n")
+        
+        print(f"-xp  PASSWORD")
+        print(f"    Alias for -o password=PASSWORD\n")
+        
+        print(f"-o   username=USERNAME (synonym for -xu)")
+        print(f"    These options allow specification of a username to be used with SMTP")
+        print(f"    servers that require authentication.  If a username is specified but a")
+        print(f"    password is not, you will be prompted to enter one at runtime.\n")
+        
+        print(f"-o   password=PASSWORD (synonym for -xp)")
+        print(f"    These options allow specification of a password to be used with SMTP")
+        print(f"    servers that require authentication.  If a username is specified but a")
+        print(f"    password is not, you will be prompted to enter one at runtime.\n")
+        
+        print(f"-o   tls=<auto|yes|no>")
+        print(f"    This option allows you to specify if TLS should be enabled or disabled.")
+        print(f"    The default, auto, will use TLS automatically if your Python installation")
+        print(f"    has SSL support and the remote SMTP server supports TLS.  To require TLS")
+        print(f"    for message delivery set this to yes.  To disable TLS support set this to no.\n")
+        
+        print(f"-o   timeout=SECONDS")
+        print(f"    This option sets the timeout value in seconds used for all network reads,")
+        print(f"    writes, and a few other things.\n")
+        
+        print(f"-o   fqdn=FQDN")
+        print(f"    This option sets the Fully Qualified Domain Name used during the initial")
+        print(f"    SMTP greeting.  Normally this is automatically detected, but in case you")
+        print(f"    need to manually set it for some reason, you can use this to override the default.\n")
+    
+    else:
+        print(f"\n{Colors.RED}ERROR => The help topic '{topic}' is not valid!{Colors.NORMAL}\n")
+        print(f"Valid topics are: addressing, message, networking, output, misc\n")
+        sys.exit(1)
+    
+    sys.exit(0)
+
 def main():
+    # Check for help options before argparse
+    if '--help' in sys.argv or '-h' in sys.argv:
+        if len(sys.argv) > 2 and sys.argv[1] in ['--help', '-h']:
+            topic = sys.argv[2]
+            show_help_topic(topic)
+        else:
+            show_help()
+    
+    # Show help if no arguments provided
+    if len(sys.argv) < 2:
+        show_help()
+    
     parser = argparse.ArgumentParser(
         description=f'{PROGRAM_NAME}-{VERSION} by {AUTHOR_NAME} <{AUTHOR_EMAIL}>',
         formatter_class=argparse.RawDescriptionHelpFormatter,
+        add_help=False,
         epilog="""
 Examples:
   sendEmail.py -f sender@example.com -t recipient@example.com -s mail.example.com -u "Subject" -m "Message body"
@@ -551,7 +865,7 @@ Examples:
     )
     
     # Required arguments
-    parser.add_argument('-f', '--from', dest='sender', required=True, help='From email address')
+    parser.add_argument('-f', '--from', dest='sender', default='', help='From email address')
     
     # Recipients
     parser.add_argument('-t', '--to', dest='to', action='append', default=[], help='To email address(es)')
@@ -626,6 +940,10 @@ Examples:
             config.dsnopts = 'NOTIFY=SUCCESS,FAILURE,DELAY'
     
     # Set sender data
+    if not args.sender:
+        print(f"{Colors.RED}ERROR => You must specify a 'from' field! Try --help.{Colors.NORMAL}")
+        sys.exit(1)
+    
     sender.sender = args.sender
     sender.to_addresses = []
     for to in args.to:
